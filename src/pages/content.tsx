4df41ef8ca88603e5249ea134cba6e466b07ddc2
GuedesThi <thiagodesignguedes@gmail.com>
2024-10-18 17:40:06 -0300

    using `react-slick` for carousel

--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -1,3 +1,83 @@
+import Slider from "react-slick";
+import styles from "../styles/content.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        speed: 400,
+        infinite: true,
+        dots: true,
+      },
+    },
+  ],
+};
+
 export default function Content() {
-  return <div>Conteúdo</div>;
+  return (
+    <div>
+      <Slider {...settings} className={styles.customCarousel}>
+        <div className={styles.card}>
+          <img
+            className={styles.carouselImage}
+            src="/beijo-restaurante.jpg"
+            alt="beijo-restaurante"
+          />
+          <p className={styles.carouselText}>2023</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.carouselImage}
+            src="/caminhada-praia.jpg"
+            alt="caminhada-praia"
+          />
+          <p className={styles.carouselText}>2023</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.carouselImage}
+            src="/deitados-filtro.jpg"
+            alt="deitados-filtro"
+          />
+          <p className={styles.carouselText}>2024</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.carouselImage}
+            src="/poze-restaurante.jpg"
+            alt="poze-restaurante"
+          />
+          <p className={styles.carouselText}>2023</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.carouselImage}
+            src="/tesla-dormindo.jpg"
+            alt="tesla-dormindo"
+          />
+          <p className={styles.carouselText}>2024</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.carouselImage}
+            src="/caminhada-praia.jpg"
+            alt="caminhada-praia"
+          />
+          <p className={styles.carouselText}>2023</p>
+        </div>
+      </Slider>
+    </div>
+  );
 }
